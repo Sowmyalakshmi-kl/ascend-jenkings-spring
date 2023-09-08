6adb29fe2fc1701f34d73e47ca44d4f9e9d987d8
s0k05sw <sowmyalakshmi.kl@walmart.com>
2023-09-08 22:21:49 +0530

    added response

--- src/main/java/com/example/jenkinsspring/controller/HomeController.java
+++ src/main/java/com/example/jenkinsspring/controller/HomeController.java
@@ -2,11 +2,13 @@ package com.example.jenkinsspring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-	@GetMapping(path ="/home")
+	@GetMapping(path ="/home", produces="text/plain")
+	@ResponseBody
 	public String home() {
 		System.out.println("Welcome to spring home");
 		return "Welcome to spring home";
