567305828d09580615c068197bc02e4aff5b857c
s0k05sw <sowmyalakshmi.kl@walmart.com>
2023-09-08 21:31:56 +0530

    init spring

---
+++ src/main/java/com/example/jenkinsspring/JenkinsSpringApplication.java
@@ -0,0 +1,14 @@
+package com.example.jenkinsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSpringApplication.class, args);
+		System.out.println("In sowmya'a spring class");
+	}
+
+}
